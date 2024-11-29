5c156d4f30c967721f9f1e7a0b48dc827cb8a77c
Sam Kortchmar <samkortchmar@gmail.com>
2024-11-28 21:12:51 -0500

    incorporate realtime data comparison and plotting

--- transform.py
+++ transform.py
@@ -9,6 +9,13 @@ import os
 import pandas as pd
 import pytz
 import glob
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+from optimization import load_results
+
 
 pd.options.mode.copy_on_write = True 
 
@@ -22,18 +29,6 @@ WEATHER_ZONES = [
     "Southern",
     "West",
 ]
-
-
-def transform_ldf(raw_ercot_df):
-    """
-    Merge LdfDate with LdfHour to have a datetime column.
-    This data appears to only change when an event occurs;
-    otherwise, it will continue to have the same hourly factors each day.
-    Meaning that missing dates have the same factors as the last posted date.
-    So, you can fill in the missing dates with the same values as the latest hourly factors for each substation.
-    """
-    pass
-
 
 # Set timezone to Central Prevailing Time (ERCOT), which is America/Chicago (CST/CDT)
 texas_tz = pytz.timezone('America/Chicago')
@@ -305,49 +300,182 @@ def load_settlement_points():
 
 ### Realtime LMPs
 
-def process_one_realtime_lmp():
-    path = 'realtime_lmps/cdr.00012300.0000000000000000.20190101.000024.LMPSROSNODENP6788_20190101_000020.csv'
+def process_one_realtime_lmp(path):
     df = pd.read_csv(path)
     pivoted = df.pivot_table(index='SCEDTimestamp', columns='SettlementPoint', values='LMP')
 
     pivoted['datetime_local'] = pivoted.index.map(lambda x: pd.to_datetime(x, format='%m/%d/%Y %H:%M:%S'))
 
     # Reorder columns to put datetime_local at the front
-    cols = pivoted.columns.tolist()
-    cols = cols[-1:] + cols[:-1]
-    pivoted = pivoted[cols]
     # pivoted = pivoted.reset_index(drop=True)
-    return pivoted
+    d = pivoted.to_dict('records')[0]
+    return d
 
-
-
-## Works for wind and solar since they are giving prices over last hour
-def process_all_historical(input_dir, output_dir='solar'):
+def process_one_yr_realtime_lmp(year, input_dir='realtime_lmps', output_dir='realtime_lmps_by_year'):
+    year = int(year)
     t0 = time.time()
-    csv_files = sorted(glob.glob(f'{input_dir}/*.csv'))
+    csv_files = glob.glob(f'{input_dir}/*_{year}*.csv')
 
-    # Loop over all CSV files and read them into pandas dataframes
-    dfs = []
-    for i, file in enumerate(csv_files):
-        if i % 10 == 0:
-            print(f'processing file {i}/{len(csv_files)}')
+    dicts = []
+    futures = []
+    processed = 0
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        futures = [
+            executor.submit(process_one_realtime_lmp, file)
+            for file in csv_files
+        ]
 
-        # Only add the last row from the file.
-        df = pd.read_csv(file)
-        df2 = df.tail(1)
-        with_local_times = convert_to_datetimes(df2).reset_index(drop=True)
+        for future in as_completed(futures):
+            try:
+                the_dict = future.result()
+                # Can happen due to 2019 as a timestamp, getting picked up by glob
+                if the_dict['datetime_local'].year != year:
+                    continue
 
-        dfs.append(with_local_times)
+                dicts.append(the_dict)
+                processed += 1
+                if processed % 10 == 0:
+                    print(f"Got results for ({processed}/{len(csv_files)})")
+            except Exception as e:
+                print(f'Error occurred: {e}')
 
     t1 = time.time()
-    print(f"Read all csvs in {t1 - t0}")
+    merged = pd.DataFrame(dicts)
+    merged.sort_values('datetime_local', inplace=True)
+    merged.to_csv(f"{output_dir}/realtime_lmp_{year}.csv", index=False)
+    print(f"merged, sorted, wrote in {t1 - t0}s")
+    return merged
 
-    merged = pd.concat(dfs, ignore_index=True)
-    t2 = time.time()
-    print(f"Concatenated in in {t2 - t1}")
+def avg_hourly_prices(df):
+    df['datetime_local'] = pd.to_datetime(df['datetime_local'], format='ISO8601')
+    df['hour'] = df['datetime_local'].dt.hour
+    df['day'] = df['datetime_local'].dt.day
+    df['month'] = df['datetime_local'].dt.month
+    df['year'] = df['datetime_local'].dt.year
 
-    os.makedirs(output_dir, exist_ok=True)
+    hourly_avg = df.groupby(['year', 'month', 'day', 'hour']).mean()
+    hourly_avg.reset_index(inplace=True)
+    hourly_avg['datetime_local'] = pd.to_datetime(hourly_avg[['year', 'month', 'day', 'hour']])
 
-    split_by_year(output_dir + '/' + output_dir + '.csv', df=merged)
-    t3 = time.time()
-    print(f"Wrote to disk in {t3 - t2}")
+    result = hourly_avg.drop(columns=['year', 'month', 'day', 'hour'])
+    dna = result.dropna(axis='columns')
+    return dna
+
+def compare_dam_with_actual(opt_results, actual, node):
+    """
+    Compare the actual LMPs with the DAM predictions.
+    """
+
+    opt_result = opt_results[node]
+    realtime_prices = actual[node]
+
+    # Plot the dam and actual against the time
+    x_times = range(365 * 24 - 1)
+
+    expected_net_profit = 0
+    actual_net_profit = 0
+
+    actual_profit_timesteps = []
+
+    for t in x_times:
+        charge_amt = opt_result['charge_schedule'][t]
+        discharge_amt = opt_result['discharge_schedule'][t]
+        expected_profit_timestep = opt_result['profit_timestep'][t]
+        actual_profit_timestep = realtime_prices[t] * (discharge_amt - charge_amt)
+        expected_net_profit += expected_profit_timestep
+        actual_net_profit += actual_profit_timestep
+        actual_profit_timesteps.append(actual_profit_timestep)
+        
+    print(f"Expected net profit: {expected_net_profit}")
+    print(f"Actual net profit: {actual_net_profit}")
+    print(f"Net profit difference: {abs(actual_net_profit - expected_net_profit)}")
+
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1, subplot_titles=('Prices', 'Profits'))
+
+    fig.add_trace(go.Scatter(
+        x=list(x_times), 
+        y=opt_result['node_lmp'], 
+        mode='lines', 
+        name='DAM Price',
+        line=dict(color='blue')
+    ),  row=1, col=1)
+    fig.add_trace(go.Scatter(
+        x=list(x_times), 
+        y=realtime_prices, 
+        mode='lines', 
+        name='Actual Price',
+        line=dict(color='red')
+    ),  row=1, col=1)
+
+    fig.add_trace(go.Scatter(
+        x=list(x_times), 
+        y=opt_result['profit_timestep'], 
+        mode='lines', 
+        name='Expected Profit',
+        line=dict(color='blue')
+    ),  row=2, col=1)
+    fig.add_trace(go.Scatter(
+        x=list(x_times), 
+        y=actual_profit_timesteps, 
+        mode='lines', 
+        name='Actual Profit',
+        line=dict(color='red')
+    ),  row=2, col=1)
+
+    fig.update_layout(
+        title=f'Expected {expected_net_profit:,.2f} vs Actual Profit {actual_net_profit:,.2f}',
+        xaxis_title='Time (hours)',
+        yaxis_title='Profit',
+        hovermode='x unified'
+    )
+
+    fig.update_yaxes(title_text="Price", row=1, col=1)
+    fig.update_yaxes(title_text="Profit", row=2, col=1)
+
+    fig.show()
+
+
+def compare_dam_with_actual_profits(year):
+    """
+    Compare the actual LMPs with the DAM predictions.
+    """
+    actual = pd.read_csv(f'realtime_lmps_by_year/realtime_lmp_{year}_hourly.csv')
+    results = load_results(year)
+
+    nodes = set(results.keys()).intersection(set(actual.keys()))
+
+    expected_net_profits = []
+    actual_net_profits = []
+    for node in nodes:
+        opt_result = results[node]
+        realtime_prices = actual[node]
+
+        # Plot the dam and actual against the time
+        x_times = range(8595)
+
+        expected_net_profit = 0
+        actual_net_profit = 0
+
+        try:
+            for t in x_times:
+                charge_amt = opt_result['charge_schedule'][t]
+                discharge_amt = opt_result['discharge_schedule'][t]
+                expected_profit_timestep = opt_result['profit_timestep'][t]
+                actual_profit_timestep = realtime_prices[t] * (discharge_amt - charge_amt)
+                expected_net_profit += expected_profit_timestep
+                actual_net_profit += actual_profit_timestep
+
+            expected_net_profits.append(expected_net_profit)
+            actual_net_profits.append(actual_net_profit)
+        except Exception as e:
+            print(f"Error occurred for node {node}: {e}")
+            continue
+
+    expected_net_profits = np.array(expected_net_profits)
+    actual_net_profits = np.array(actual_net_profits)
+    print(f"Across {len(nodes)} nodes in {year}")
+    print(f"\tMean optimal profit: {np.mean(expected_net_profits):,.2f}")
+    print(f"\tMean actual profit: {np.mean(actual_net_profits):,.2f}")
+
+    diff = abs(np.mean((actual_net_profits - expected_net_profits) / expected_net_profits) * 100)
+    print(f"\tActual profit as a percentage of optimal {diff:.2f}%")
